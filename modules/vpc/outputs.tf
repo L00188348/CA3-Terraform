1c0db3131f73d2955c4deb788e41a1612ce5ca35
L00188348 <L00188348@atu.ie>
2025-11-11 19:42:18 +0000

    VPC created (modules/vpc) - apply done

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_id" {
+  description = "ID da VPC criada"
+  value       = aws_vpc.this.id
+}
+
+output "internet_gateway_id" {
+  description = "ID do Internet Gateway associado à VPC"
+  value       = aws_internet_gateway.this.id
+}
+
+output "public_subnet_ids" {
+  description = "Lista de IDs das subnets públicas"
+  value       = aws_subnet.public[*].id
+}
+
+output "public_subnet_cidrs" {
+  description = "Lista de CIDRs das subnets públicas"
+  value       = aws_subnet.public[*].cidr_block
+}
+
+output "public_subnet_azs" {
+  description = "Lista de AZs usadas nas subnets públicas"
+  value       = aws_subnet.public[*].availability_zone
+}
+
+output "private_subnet_ids" {
+  description = "Lista de IDs das subnets privadas"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_subnet_cidrs" {
+  description = "Lista de CIDRs das subnets privadas"
+  value       = aws_subnet.private[*].cidr_block
+}
+
+output "private_subnet_azs" {
+  description = "Lista de AZs usadas nas subnets privadas"
+  value       = aws_subnet.private[*].availability_zone
+}
